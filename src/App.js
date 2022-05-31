2e0a3164da2cf373d0adc9b088e5b48e89ddece5
KubiGR <adranidisb@gmail.com>
2022-05-31 20:22:10 +0300

    Add chapter-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import TodoList from "./todos/TodoList";
+const App = () => {
+  const site = {
+    homepage: "www.google.com",
+  };
 
-const App = () => (
-  <div className="app">
-    <TodoList />
-  </div>
-);
+  const user = {
+    name: "Dobby",
+  };
+
+  function formatName(user) {
+    return `Banana ${user.name}`;
+  }
+
+  return (
+    <div className="app">
+      <h1>React is {5 + 5} times better with JSX</h1>
+      <a href={site.homepage}>
+        <h1>Homepage</h1>
+      </a>
+      <h1>Hello, {formatName(user)}!</h1>
+    </div>
+  );
+};
 
 export default App;
