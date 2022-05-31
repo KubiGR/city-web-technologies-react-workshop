668d299df29d78e5607972f7890724ab778f8b01
KubiGR <adranidisb@gmail.com>
2022-05-31 20:47:42 +0300

    Add chapter-3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import NewTodoForm from "./todos/NewTodoForm";
 import TodoListItem from "./todos/TodoListItem";
 
 const App = () => {
-  const todo = {
-    text: "I am a todo text",
-  };
+  const todos = [
+    {
+      id: 1,
+      text: "I am the first todo",
+    },
+    {
+      id: 2,
+      text: "I am the second todo",
+    },
+    {
+      id: 3,
+      text: "I am the third todo",
+    },
+  ];
+
   return (
     <div className="app">
       <div className="list-wrapper">
         <NewTodoForm />
         <div className="list-scrollable">
-          <TodoListItem todo={todo} />
+          <TodoListItem todo={todos[0]} />
+          <TodoListItem todo={todos[1]} />
+          <TodoListItem todo={todos[2]} />
+        </div>
+        <div className="list-scrollable">
+          {todos.map((todo) => (
+            <TodoListItem key={todo.id} todo={todo} />
+          ))}
         </div>
       </div>
     </div>
