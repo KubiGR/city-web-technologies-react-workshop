7c698ff48548b6ac29419181ad47eafe37d31571
KubiGR <adranidisb@gmail.com>
2022-06-02 23:49:54 +0300

    Add chapter-5

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import { useState, useEffect } from "react";
 
 import NewTodoForm from "./todos/NewTodoForm";
 import TodoListItem from "./todos/TodoListItem";
-import CompletedTodoListItem from "./todos/CompletedTodoListItem";
 
 const App = () => {
   const [todoList, setTodoList] = useState(
     JSON.parse(localStorage.getItem("todoList")) || []
-  );
-  const [completedTodoList, setCompletedTodoList] = useState(
-    JSON.parse(localStorage.getItem("completedTodoList")) || []
   );
 
   const [idCounter, setIdCounter] = useState(
@@ -19,10 +15,6 @@ const App = () => {
   useEffect(() => {
     localStorage.setItem("todoList", JSON.stringify(todoList));
     localStorage.setItem("idCounter", idCounter);
-    localStorage.setItem(
-      "completedTodoList",
-      JSON.stringify(completedTodoList)
-    );
     document.title = `Todos: ${todoList.length}`;
   });
 
@@ -30,38 +22,9 @@ const App = () => {
     const newTodoList = todoList.concat({
       ...todo,
       id: idCounter,
-      completed: false,
     });
     setTodoList(newTodoList);
     setIdCounter((oldIdCounter) => oldIdCounter + 1);
-  };
-
-  const removeTodo = (todo) => {
-    const newTodoList = todoList.filter((e) => todo.id !== e.id);
-    setTodoList(newTodoList);
-  };
-
-  const editTodo = (todo, newText) => {
-    const todoToEdit = todoList.find((e) => todo.id === e.id);
-    todoToEdit.text = newText;
-    setTodoList(todoList.slice());
-  };
-
-  const completeTodo = (todo) => {
-    const newTodoList = todoList.filter((e) => todo.id !== e.id);
-    setTodoList(newTodoList);
-    const newCompletedTodoList = completedTodoList.concat({
-      ...todo,
-      completed: true,
-    });
-    setCompletedTodoList(newCompletedTodoList);
-  };
-
-  const removeCompletedTodo = (todo) => {
-    const newCompletedTodoList = completedTodoList.filter(
-      (e) => todo.id !== e.id
-    );
-    setCompletedTodoList(newCompletedTodoList);
   };
 
   return (
@@ -70,20 +33,7 @@ const App = () => {
         <NewTodoForm addTodo={addTodo} />
         <div className="list-scrollable">
           {todoList.map((todo) => (
-            <TodoListItem
-              key={todo.id}
-              todo={todo}
-              removeTodo={removeTodo}
-              completeTodo={completeTodo}
-              editTodo={editTodo}
-            />
-          ))}
-          {completedTodoList.map((todo) => (
-            <CompletedTodoListItem
-              key={todo.id}
-              todo={todo}
-              removeCompletedTodo={removeCompletedTodo}
-            />
+            <TodoListItem key={todo.id} todo={todo} />
           ))}
         </div>
       </div>
--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,39 +1,17 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState } from "react";
 
 const NewTodoForm = ({ addTodo }) => {
   const [inputValue, setInputValue] = useState("");
-  const inputElementRef = useRef(null);
 
-  const createTodo = useCallback(() => {
+  const createTodo = () => {
     if (inputValue === "") return;
     addTodo({ text: inputValue });
     setInputValue("");
-    inputElementRef.current.focus();
-  }, [addTodo, inputValue]);
-
-  const handleKeydown = useCallback(
-    (evt) => {
-      if (evt.key === "Enter") {
-        createTodo();
-      }
-    },
-    [createTodo]
-  );
-
-  useEffect(() => {
-    inputElementRef.current.focus();
-
-    window.addEventListener("keydown", handleKeydown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeydown);
-    };
-  }, [handleKeydown]);
+  };
 
   return (
     <div className="new-todo-form">
       <input
-        ref={inputElementRef}
         className="new-todo-input"
         type="text"
         placeholder="Type your new todo here..."
